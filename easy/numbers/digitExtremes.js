1ec1eda9109c9a0f440f204bc583babd8d3f2b45
0xroot <sharmag.khwahish@gmail.com>
2025-07-09 17:52:26 +0530

    Reviewed code and added comments

--- easy/numbers/digitExtremes.js
+++ easy/numbers/digitExtremes.js
@@ -1,19 +1,19 @@
 //finding largest and smallest digits in a number
 
     function findExtremes(num){
-        num = Math.abs(num);
-        let largest = 0;
-        let smallest = 9;
-            while(num>0){
-                let digit = num%10;
-                if(digit>largest){
+        num = Math.abs(num);    //converting num to positive to gnore -ve sign if present
+        let largest = 0;    //initialized largest as 0 as its the min possible digit
+        let smallest = 9;   //initialized smallest as 9 as its the max possible digit
+            while(num>0){   
+                let digit = num%10; //collecting the last digit
+                if(digit>largest){  //if > 0 so far, we'll update largest
                     largest=digit;
                 }
-                if(digit<smallest){
+                if(digit<smallest){ //if < 9 so far, we'll update smallest
                     smallest=digit;
                 }
-                num = Math.floor(num/10);
+                num = Math.floor(num/10);   //removing the last digit
             }
-        return {largest, smallest};
+        return {largest, smallest}; //return results in an object
     }
     console.log(findExtremes(2345));
