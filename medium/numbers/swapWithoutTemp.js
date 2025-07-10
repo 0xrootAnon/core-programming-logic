9be88f2979d6905dd3374628d1cc5f59f754ea22
0xroot <sharmag.khwahish@gmail.com>
2025-07-10 09:36:33 +0530

    Swapping without taking extra space

---
+++ medium/numbers/swapWithoutTemp.js
@@ -0,0 +1,28 @@
+// swapping without using a third variable
+
+// Method 1: using add & subtract
+let a = 5;
+let b = 7;
+a = a + b;  // a = 12
+b = a - b;  // b = 5 (original a)
+a = a - b;  // a = 7 (original b)
+console.log("a =", a);  // 7
+console.log("b =", b);  // 5
+
+// Method 2: using multiplication and devision
+let c = 8;
+let d = 2;
+c = c * d;  // c = 16
+d = c / d;  // d = 8
+c = c / d;  // c = 2
+console.log("c =", c);  // 2
+console.log("d =", d);  // 8
+
+//Method 3: using bitwise XOR
+let e = 5; // binary: 0101
+let f = 7; // binary: 0111
+e = e ^ f;  // e = 0101 ^ 0111 = 0010 (2)
+f = e ^ f;  // f = 0010 ^ 0111 = 0101 (5)
+e = e ^ f;  // e = 0010 ^ 0101 = 0111 (7)
+console.log("e =", e);  // Output: 7
+console.log("f =", f);  // Output: 5
