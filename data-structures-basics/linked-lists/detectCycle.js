f4956f88af44d2fc1cd5ebbe382ef0b2d22fd5f8
0xroot <sharmag.khwahish@gmail.com>
2025-07-13 02:40:22 +0530

    DSA

---
+++ data-structures-basics/linked-lists/detectCycle.js
@@ -0,0 +1,11 @@
+//floyd’s tortoise & hare
+
+function hasCycle(head) {
+  let slow = head, fast = head;
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+    if (slow === fast) return true;
+  }
+  return false;
+}
