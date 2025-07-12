fdb086ca3cef259f6a922171be43e9e78094fac1
0xroot <sharmag.khwahish@gmail.com>
2025-07-12 19:04:47 +0530

    removing duplicate characters

---
+++ medium/strings/removeDuplicates.js
@@ -0,0 +1,19 @@
+//Remove duplicate characters 
+
+//method 1: using set
+function removeDuplicates(str) {
+  return [...new Set(str)].join('');
+}
+
+//method 2: loop and lookup 
+function removeDuplicatesManual(str) {
+  let seen = {};
+  let result = '';
+  for (let char of str) {
+    if (!seen[char]) {
+      result += char;
+      seen[char] = true;
+    }
+  }
+  return result;
+}
