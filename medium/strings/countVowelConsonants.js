88c131182e906e79db652c8be218d291df917061
0xroot <sharmag.khwahish@gmail.com>
2025-07-12 19:02:49 +0530

    count em for me ahh problem

---
+++ medium/strings/countVowelConsonants.js
@@ -0,0 +1,13 @@
+//Count vowels/consonants 
+
+function countVowelsConsonants(str) {
+  let vowels = 0, consonants = 0;
+  const s = str.toLowerCase();
+  for (let char of s) {
+    if (/[a-z]/.test(char)) {
+      if ('aeiou'.includes(char)) vowels++;
+      else consonants++;
+    }
+  }
+  return { vowels, consonants };
+}
