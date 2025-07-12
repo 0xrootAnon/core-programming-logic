3a2f9ccd957393acb7b39fcd7ffbe914c8935134
0xroot <sharmag.khwahish@gmail.com>
2025-07-12 19:48:21 +0530

    longest palindromic substring

---
+++ hard/strings/longestPalindrome.js
@@ -0,0 +1,22 @@
+//Longest Palindromic Substring 
+
+//method: expand around corner
+function longestPalindromicSubstring(s) {
+  let start = 0, end = 0;
+  const expand = (l, r) => {
+    while (l >= 0 && r < s.length && s[l] === s[r]) {
+      l--; r++;
+    }
+    return [l + 1, r - 1];
+  };
+
+  for (let i = 0; i < s.length; i++) {
+    let [l1, r1] = expand(i, i);     // Odd length
+    let [l2, r2] = expand(i, i + 1); // Even length
+    if (r1 - l1 > end - start) [start, end] = [l1, r1];
+    if (r2 - l2 > end - start) [start, end] = [l2, r2];
+  }
+  return s.slice(start, end + 1);
+}
+
+
