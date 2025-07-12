ec12e13beb89f39bb35ac1d88972797b7ae36d70
0xroot <sharmag.khwahish@gmail.com>
2025-07-12 18:47:01 +0530

    reversing a string

---
+++ easy/strings/reverseString.js
@@ -0,0 +1,18 @@
+//reversing strings
+
+//method 1: Using split().reverse().join()
+function reverseString(str) {
+  return str.split('').reverse().join('');
+}
+
+//method 2: Manual loop
+function reverseStringManual(str) {
+  let reversed = '';
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed += str[i];
+  }
+  return reversed;
+}
+
+console.log(reverseStringManual("khwahish"));
+console.log(reverseString("khwahish"));
