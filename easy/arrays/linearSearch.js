74e98b82f0790cfe53a5969d57e7be146fda5b6f
0xroot <sharmag.khwahish@gmail.com>
2025-07-12 20:11:38 +0530

    Searching for an elememt

---
+++ easy/arrays/linearSearch.js
@@ -0,0 +1,8 @@
+//Search for an element (linear search) 
+
+function linearSearch(arr, target) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === target) return i;
+  }
+  return -1;
+}
