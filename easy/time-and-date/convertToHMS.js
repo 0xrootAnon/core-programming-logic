bc607221aa9d4a37e5581c5815ce5ff420da3e61
0xroot <sharmag.khwahish@gmail.com>
2025-07-12 21:22:20 +0530

    convert seconds to hr:mins:secs

---
+++ easy/time-and-date/convertToHMS.js
@@ -0,0 +1,8 @@
+//convert seconds to hours:minutes:seconds 
+
+function convertSecondsToHMS(seconds) {
+  const hrs = Math.floor(seconds / 3600);
+  const mins = Math.floor((seconds % 3600) / 60);
+  const secs = seconds % 60;
+  return `${hrs}h ${mins}m ${secs}s`;
+}
