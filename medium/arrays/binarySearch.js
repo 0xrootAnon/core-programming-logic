da9645982ce662caa5dcd0920021e5160bc4cdf3
0xroot <sharmag.khwahish@gmail.com>
2025-07-12 20:22:51 +0530

    iterative binary search

---
+++ medium/arrays/binarySearch.js
@@ -0,0 +1,11 @@
+//iterative Binary Search (sorted array)
+function binarySearch(arr, target) {
+  let low = 0, high = arr.length - 1;
+  while (low <= high) {
+    let mid = Math.floor((low + high) / 2);
+    if (arr[mid] === target) return mid;
+    if (arr[mid] < target) low = mid + 1;
+    else high = mid - 1;
+  }
+  return -1;
+}
